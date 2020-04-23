8f02b3057ec3af1e87979ae4adab26e0f7fa9479
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-04-22 22:40:03 -0400

    substring-project added test case for bdd

--- lib/substring.rb
+++ lib/substring.rb
@@ -1,3 +1,8 @@
 class Substring
 
+    def initialize(word, wordArray)
+    end
+
 end
+s = Substring.new("hello world", ["hello"])
+puts s
--- test/substring_spec.rb
+++ test/substring_spec.rb
@@ -1,5 +1,10 @@
 require 'rspec'
+require 'rspec/autorun'
 require './lib/substring'
 
-describe('#substring') do
+describe Substring do
+    it 'returns substrings' do
+        s = Substring.new("Hello World", ["Hello", "World", "Toast"])
+        expect(s).to eq({"Hello" => 1, "World" => 1})
+    end
 end
