87b329fc9dbbcaaa3ef1e5c8003f5e3fa625a1ab
Randolph Cisneros <cisnerosRandolph@gmail.com>
2020-04-23 19:54:57 -0400

    substring-project finished

--- lib/substring.rb
+++ lib/substring.rb
@@ -1,8 +1,20 @@
 class Substring
 
-    def initialize(word, wordArray)
-    end
+    def substrings word, subs
 
+        wordMap = {}
+
+        subs.each do |s|
+            count = 0
+            loopWord = word.downcase
+            while loopWord.include?(s.downcase)
+                count += 1
+                loopWord = loopWord.sub(s.downcase,'')
+            end
+            if count > 0
+                wordMap[s] = count
+            end
+        end
+        return wordMap
+    end
 end
-s = Substring.new("hello world", ["hello"])
-puts s
--- test/substring_spec.rb
+++ test/substring_spec.rb
@@ -3,8 +3,25 @@ require 'rspec/autorun'
 require './lib/substring'
 
 describe Substring do
+    before(:each) do
+        @s = Substring.new
+    end
+
     it 'returns substrings' do
-        s = Substring.new("Hello World", ["Hello", "World", "Toast"])
-        expect(s).to eq({"Hello" => 1, "World" => 1})
+        expect(@s.substrings("Hello World", ["Hello", "World"])).to eq({"Hello" => 1, "World" => 1})
+    end
+
+    it 'returns substrings' do
+        expect(@s.substrings("Hello World, World", ["Hello", "World"])).to eq({"Hello" => 1, "World" => 2})
+    end
+
+    it 'gets count for dictionary' do
+        dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+        expect(@s.substrings("below", dictionary)).to eq({"below" => 1, "low" => 1})
+    end
+
+    it 'works for multiple words' do
+        dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+        expect(@s.substrings("Howdy partner, sit down! How's it going?", dictionary)).to eq({"down" => 1, "how" => 2, "howdy" => 1,"go" => 1, "going" => 1, "it" => 2, "i" => 3, "own" => 1,"part" => 1,"partner" => 1,"sit" => 1})
     end
 end
